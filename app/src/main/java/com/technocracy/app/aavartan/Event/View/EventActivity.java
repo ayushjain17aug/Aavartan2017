8f402669712b366165fe69d1d5bd29b395b981c3
abhi <meabhi75@gmail.com>
2017-09-26 11:08:24 +0530

    Events and Schedule Completed and Tested

--- app/src/main/java/com/technocracy/app/aavartan/Event/View/EventActivity.java
+++ app/src/main/java/com/technocracy/app/aavartan/Event/View/EventActivity.java
@@ -26,6 +26,7 @@ public class EventActivity extends AppCompatActivity implements EventView {
     private EventAdapter adapter;
     private EventPresenter presenter;
     private String eventSetId;
+    private String toolbarTitle = "";
     private List<Event> eventList;
 
     @Override
@@ -33,21 +34,28 @@ public class EventActivity extends AppCompatActivity implements EventView {
         super.onCreate(savedInstanceState);
         Bundle data = getIntent().getExtras();
         eventSetId = data.getString("event_selected");
-        if (eventSetId.equals("1"))
+        if (eventSetId.equals("1")) {
             eventSetId = "fun";
-        else if (eventSetId.equals("2"))
+            toolbarTitle = "   Fun";
+        }
+        else if (eventSetId.equals("2")) {
             eventSetId = "manager";
-        else if (eventSetId.equals("3"))
+            toolbarTitle = "   Managerial";
+        }
+        else if (eventSetId.equals("3")) {
             eventSetId = "tech";
-        else
+            toolbarTitle = "  Technical";
+        }
+        else {
             eventSetId = "robo";
-        
+            toolbarTitle = "   Robotics";
+        }
+        Log.d("event_set_id", eventSetId);
         setContentView(R.layout.activity_event);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(toolbarTitle);
+
         setSupportActionBar(toolbar);
-        //getSupportActionBar().setTitle(eventSetId);
-//        toolbar.setSubtitleTextColor(Color.WHITE);
-      //  setSupportActionBar(toolbar);
         progressBar = (ProgressBar) findViewById(R.id.progress_bar_event);
         recyclerView = (RecyclerView) findViewById(R.id.event_recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
